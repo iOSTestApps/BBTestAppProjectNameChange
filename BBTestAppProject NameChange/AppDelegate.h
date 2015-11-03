eafb959ff4c2f6cc5e6ff001144a9ce4fb10b473
Christopher Stott <christopherstott@gmail.com>
2015-11-02 19:33:10 -0800

    Initial Commit

---
+++ BBTestAppProject NameChange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTestAppProject NameChange
+//
+//  Created by Chris on 2015-11-02.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBTestAppProject NameChange/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BBTestAppProject NameChange
+//
+//  Created by Chris on 2015-11-02.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
